5284207eda45b4671ca6da86cc53b4a20f7c5aaa
Schausi <schausi19993@gmail.com>
2017-08-07 09:35:21 +0200

    Small Changes

--- DB_Files/Create_Script.sql
+++ DB_Files/Create_Script.sql
@@ -6,22 +6,19 @@ CREATE TABLE Types(
 	TY_NAME VARCHAR(30) NOT NULL,
 	PRIMARY KEY (TY_ID)
 );
-Insert into Types (TY_NAME) VALUES ('Room'),('Laptop'),('Dockingstation');/*...*/
+
+Insert into Types (TY_NAME) VALUES ('Room'),('Laptop'),('Dockingstation');
 
 CREATE TABLE Tags (
      T_ID Long NOT NULL,
 	 T_TYPE INT,
      T_NAME VARCHAR(30) NOT NULL,
 	 T_LASTFOUND Date,
-	 T_FOUND_IN_LAST_RUN Boolean,
+	 T_FOUND_IN_LAST_RUN Boolean NOT NULL,
      PRIMARY KEY (T_ID),
-	 CONSTRAINT 'FK_TAG_TYPE'
-		FOREIGN KEY (T_ID) REFERENCES TYPE (TY_ID)
-	 
+	 CONSTRAINT FK_TAG_TYPE
+		FOREIGN KEY (T_TYPE) REFERENCES Types (TY_ID)	 
  );
 
-INSERT INTO Tags (T_ID,T_NAME) VALUES
-    (1,'dog');
-/*INSERT INTO Tags (T_ID,T_NAME) VALUES
-    ('dog'),('cat'),('penguin'),
-    ('fox'),('whale'),('ostrich');*/
+INSERT INTO Tags (T_ID,T_TYPE,T_NAME) VALUES
+    (1,2,'dog');
